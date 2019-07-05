1b1f74867eb67febc79ede73065d58378c8d0e4a
mpascual98 <manu.pascual98@gmail.com>
2019-07-05 13:00:48 -0300

    Login migrado.

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,73 +1,157 @@
-@extends('layouts.app')
+@extends('plantilla_login')
 
-@section('content')
+<style>
+body{
+  color: yellow;
+  background-image: url(IMG0/imgfondo.jpg);
+}
+
+input, select, textarea{
+    color: yellow;
+}
+* {
+
+font-family: sans-serif;
+box-sizing: border-box;
+}
+
+
+.container{
+display: flex;
+justify-content: center;
+align-items: center;
+}
+
+form {
+margin: auto;
+width: 50%;
+max-width: 400px;
+background-color: white;
+padding: 20px;
+border: 1px solid;
+text-align: center;
+background-color: black;
+color: yellow;
+
+}
+form.caja{
+margin-top: 200px;
+}
+
+.login {
+text-align: center;
+margin-bottom: 10px;
+}
+
+.pass {
+text-align: center;
+}
+
+h2 {
+text-align: center;
+margin-bottom: 20px
+color: rgba(0, 0, 0, 0.5);
+}
+
+.dato1{
+border: 1px solid yellow;
+background-color: inherit;
+display: block;
+width: 100%;
+padding: 10px;
+font-size: 15px;
+}
+.recordar{
+margin-top: 10px;
+display: flex;
+flex-wrap: wrap;
+text-align: center;
+justify-content: center;
+}
+input[type="submit"] {
+
+background:rgb(255,250,205);
+border: 0;
+color: black;
+cursor: pointer;
+border-radius: 20px;
+}
+
+button[type="button"] {
+
+border: 0;
+color: red;
+cursor: pointer;
+border-radius: 20px;
+}
+
+a:-webkit-any-link {
+    color: yellow;
+    cursor: pointer;
+    text-decoration: none;
+}
+
+
+</style>
+
+
+@section('prueba')
+
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Login') }}</div>
 
-                <div class="card-body">
-                    <form method="POST" action="{{ route('login') }}">
+                    <form method="POST" action="{{ route('login') }}" class="caja">
                         @csrf
+                        <div class="formulario">
+      <h2>{{ __('Iniciar sesion') }}</h2>
 
-                        <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
+                            {{-- <label for="email">{{ __('E-Mail Address') }}</label> --}}
 
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+
+                                <input id="email" type="email" class="dato1 @error('email') is-invalid @enderror" name="email" placeholder="Ingrese Email" value="{{ old('email') }}" required autocomplete="email" autofocus >
 
                                 @error('email')
                                     <span class="invalid-feedback" role="alert">
                                         <strong>{{ $message }}</strong>
                                     </span>
                                 @enderror
-                            </div>
-                        </div>
 
-                        <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
 
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
+<br>
+
+                            {{-- <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label> --}}
+
+
+                                <input id="password" type="password" placeholder="Contraseña" class="dato1 @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
 
                                 @error('password')
                                     <span class="invalid-feedback" role="alert">
                                         <strong>{{ $message }}</strong>
                                     </span>
                                 @enderror
-                            </div>
-                        </div>
 
-                        <div class="form-group row">
-                            <div class="col-md-6 offset-md-4">
-                                <div class="form-check">
+
+<br>
                                     <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
 
-                                    <label class="form-check-label" for="remember">
-                                        {{ __('Remember Me') }}
+                                    <label class="" for="remember">
+                                        {{ __('Recordarme') }}
                                     </label>
-                                </div>
-                            </div>
-                        </div>
 
-                        <div class="form-group row mb-0">
-                            <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Login') }}
-                                </button>
+
+
+<br>
+<br>
+                                <input type="submit" class="dato1" value="Ingresar">
+                                    {{-- {{ __('Log In') }} --}}
+                                </input>
+<br>
 
                                 @if (Route::has('password.request'))
-                                    <a class="btn btn-link" href="{{ route('password.request') }}">
-                                        {{ __('Forgot Your Password?') }}
+                                    <a class="" href="{{ route('password.request') }}">
+                                        {{ __('Recordar contraseña') }}
                                     </a>
                                 @endif
-                            </div>
+
                         </div>
-                    </form>
-                </div>
-            </div>
-        </div>
-    </div>
 </div>
 @endsection
